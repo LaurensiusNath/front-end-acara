22837525b81dab409d5ba3b632b09d001f18f5d3
LaurensiusNath <nathan.surya0332@gmail.com>
2025-04-15 22:23:05 +0700

    feat: register

---
+++ src/pages/auth/register/index.tsx
@@ -0,0 +1,14 @@
+import AuthLayout from "@/components/layouts/AuthLayout";
+import Register from "@/components/views/Register";
+
+import React from "react";
+
+const RegisterPage = () => {
+  return (
+    <AuthLayout title="Acara | Register">
+      <Register />
+    </AuthLayout>
+  );
+};
+
+export default RegisterPage;
---
+++ src/pages/auth/register/success/index.tsx
@@ -0,0 +1,14 @@
+import AuthLayout from "@/components/layouts/AuthLayout";
+import Register from "@/components/views/Register";
+
+import React from "react";
+
+const RegisterSuccessPage = () => {
+  return (
+    <AuthLayout title="Acara | Register Success">
+      <h1>Register Success</h1>
+    </AuthLayout>
+  );
+};
+
+export default RegisterSuccessPage;
