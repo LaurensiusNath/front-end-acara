49296666ea4a652b1e6f4e5e3b1a9a9ca6802948
LaurensiusNath <nathan.surya0332@gmail.com>
2025-05-24 10:57:46 +0700

    feat: user activation

--- src/pages/auth/register/success/index.tsx
+++ src/pages/auth/register/success/index.tsx
@@ -1,12 +1,11 @@
 import AuthLayout from "@/components/layouts/AuthLayout";
-import Register from "@/components/views/Register";
-
+import RegisterSuccess from "@/components/views/Auth/RegisterSuccess";
 import React from "react";
 
 const RegisterSuccessPage = () => {
   return (
     <AuthLayout title="Acara | Register Success">
-      <h1>Register Success</h1>
+      <RegisterSuccess />
     </AuthLayout>
   );
 };
