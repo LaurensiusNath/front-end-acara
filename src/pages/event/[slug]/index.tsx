fd559571ea320c4fb8978adb9541135cdc46975c
LaurensiusNath <nathan.surya0332@gmail.com>
2025-06-03 17:20:18 +0700

    feat: detail event & set up payment

---
+++ src/pages/event/[slug]/index.tsx
@@ -0,0 +1,10 @@
+import LandingPageLayout from "@/components/layouts/LandingPageLayout";
+import DetailEvent from "@/components/views/DetailEvent";
+
+export default function DetailEventPage() {
+  return (
+    <LandingPageLayout title="Acara - Home">
+      <DetailEvent />
+    </LandingPageLayout>
+  );
+}
